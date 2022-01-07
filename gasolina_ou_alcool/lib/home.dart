cccf31d0685e036adad1016ddee80e79a0782770
pdr-dev71 <joaopedrosr2699@gmail.com>
2022-01-07 15:33:54 -0300

    added scroll

--- gasolina_ou_alcool/lib/home.dart
+++ gasolina_ou_alcool/lib/home.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: file_names
-
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -12,49 +10,86 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Álcool ou Gasolina",
         ),
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 32),
-              child: Image.asset("images/logo.png"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child:
-                  Text("Saiba qual a melhor opção para abastacer o seu veiculo",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      )),
-            ),
-            TextField(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 32),
+                child: Image.asset("images/logo.png"), //Logo da aplicação
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para abastacer o seu veiculo",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   labelText: "Preço Alcool, ex: 1.59",
                 ),
                 style: TextStyle(
-                  fontSize: 22,
-                )),
-            TextField(),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: RaisedButton(
-                child: Text("Calcular"),
-                onPressed: () => {},
+                  fontSize: 20,
+                ),
+                //controller: _controllerAlcool,
               ),
-            ),
-          ],
+              const TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço Gasolina, ex: 5.60",
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                //controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: const Text(
+                    "Calcular",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  onPressed: () => {},
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 17),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
